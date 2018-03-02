2a028c8148f2bc4778053ec45d91a22df496b646
Peter Dunworth - OIT <peter.dunworth@state.co.us>
2018-03-01 17:58:39 -0800

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # CpsTool
-Encryption tool for configuration property service.
+Encryption tool for configuration property service. This is a java command line tool to see help, run the command:
+
+```
+java -jar target/CpsTool-1.0.0.jar
+```
+
+Some examples of using the tool from a bash shell are:
+
+## To pull and decrypt
+curl -k -H "client_id:bc80c73ad5fb4f21b4fc620c443eecac" -H "client_secret:D2Df58ae2a0f46e6882A69D51463e411" https://localhost:9084/configuration-property-service/v1/config/base/base/base/base/testkey | java -jar target/CpsTool-1.0.0.jar decrypt
+
+## To pull and encrypt
+curl -k -H "client_id:bc80c73ad5fb4f21b4fc620c443eecac" -H "client_secret:D2Df58ae2a0f46e6882A69D51463e411" https://localhost:9084/configuration-property-service/v1/config/base/base/base/base/testkey | java -jar target/CpsTool-1.0.0.jar encrypt
+
+## To pull, encrypt and push
+curl -k -H "client_id:bc80c73ad5fb4f21b4fc620c443eecac" -H "client_secret:D2Df58ae2a0f46e6882A69D51463e411" https://localhost:9084/configuration-property-service/v1/config/base/base/base/base/testkey | java -jar target/CpsTool-1.0.0.jar encrypt | curl -k -H "client_id:bc80c73ad5fb4f21b4fc620c443eecac" -H "client_secret:D2Df58ae2a0f46e6882A69D51463e411" -H "edit_token:xx" -H "Content-Type:application/json" -X POST --data @-  https://localhost:9084/configuration-property-service/v1/config
+
+## To pull, rotate key and push
+curl -k -H "client_id:bc80c73ad5fb4f21b4fc620c443eecac" -H "client_secret:D2Df58ae2a0f46e6882A69D51463e411" https://localhost:9084/configuration-property-service/v1/config/base/base/base/base/testkey | java -jar target/CpsTool-1.0.0.jar rotateKey testkey2 | curl -k -H "client_id:bc80c73ad5fb4f21b4fc620c443eecac" -H "client_secret:D2Df58ae2a0f46e6882A69D51463e411" -H "edit_token:xx" -H "Content-Type:application/json" -X POST --data @-  https://localhost:9084/configuration-property-service/v1/config
+
+## Required environment variables
+The encryption depends on keys stored in a keystore file. The keys are RSA key pairs where the keystore alias is the keyId. To point to the correct keystore, set these variables:
+
+mule_cps_keystore_filename=<path to keystore.jks file>
+mule_cps_keystore_password=<the keystore's password (defaults to "")>
+mule_cps_key_password=<the password for the keys (note that all the passwords use must have the same password)>
---
+++ src/main/java/com/mulesoft/java/CpsTool.java
@@ -0,0 +1,194 @@
+package com.mulesoft.java;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import org.apache.commons.io.IOUtils;
+import org.mule.consulting.cps.encryption.CpsEncryptor;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.type.MapType;
+import com.fasterxml.jackson.databind.type.TypeFactory;
+
+public class CpsTool {
+
+	public static void main(String[] args) throws Exception {
+		try {
+			if (args.length <= 0) {
+				printHelp();
+			} else if (args[0].equals("decrypt")) {
+				String data = IOUtils.toString(System.in, "UTF8");
+				String json = decrypt(data);
+				System.out.println(json);
+			} else if (args[0].equals("encrypt")) {
+				String argKeyId = (args.length > 1) ? args[1] : null;
+				String data = IOUtils.toString(System.in, "UTF8");
+				String json = encrypt(argKeyId, data);
+				System.out.println(json);
+			} else if (args[0].equals("rotateKey")) {
+				String argKeyId = (args.length > 1) ? args[1] : null;
+				String data = IOUtils.toString(System.in, "UTF8");
+				String jsonData = decrypt(data);
+				String json = encrypt(argKeyId, jsonData);
+				System.out.println(json);
+			}
+		} catch (Exception e) {
+			e.printStackTrace(System.err);
+			System.exit(500);
+		}
+	}
+
+	private static String encrypt(String argKeyId, String data) throws Exception {
+		Map<String, Object> payload;
+		ObjectMapper mapper;
+		TypeFactory factory;
+		MapType type;
+
+		factory = TypeFactory.defaultInstance();
+		type = factory.constructMapType(LinkedHashMap.class, String.class, Object.class);
+		mapper = new ObjectMapper();
+		payload = mapper.readValue(data, type);
+
+		/* Start determining keyId */
+		String keyId = (String) payload.get("keyId");
+		String cipherKey = (String) payload.get("cipherKey");
+		if (keyId == null || keyId.isEmpty()) {
+			keyId = argKeyId;
+		}
+		if (keyId == null || keyId.isEmpty()) {
+			String msg = "Need a keyId to be specified in the configuration file, cannot continue with encrypt";
+			System.err.println(msg);
+			throw new Exception(msg);
+		}
+
+		Map<String, String> properties = (Map<String, String>) payload.get("properties");
+		boolean priorEncryptions = false;
+		for (String key : properties.keySet()) {
+			String value = properties.get(key);
+			if (value.startsWith("![")) {
+				priorEncryptions = true;
+			}
+		}
+		if (!priorEncryptions && argKeyId != null) {
+			keyId = argKeyId;
+			payload.put("keyId", keyId);
+		}
+		/* End determining keyId */
+
+		/* Start determining cipherKey */
+		boolean newCipherKeyGenerated = false;
+		CpsEncryptor cpsEncryptor = null;
+		if (cipherKey == null || cipherKey.isEmpty()) {
+			String msg = "Generating a new cipherKey for encryption";
+			System.err.println(msg);
+			cpsEncryptor = new CpsEncryptor(keyId);
+			cipherKey = cpsEncryptor.getCpsKey().getCipherKey();
+			newCipherKeyGenerated = true;
+		} else {
+			cpsEncryptor = new CpsEncryptor(keyId, cipherKey);
+		}
+		/* End determining cipherKey */
+
+		String securePropertyList = properties.get("secure.properties");
+		if (securePropertyList == null || securePropertyList.isEmpty()) {
+			/* Nothing to do */
+			System.err.println("No properties listed in secure.properties");
+		} else {
+			String[] secureProperties = securePropertyList.split(",");
+			boolean canEncrypt = true;
+			for (String key : secureProperties) {
+				String plainTextValue = properties.get(key.trim());
+				if (cpsEncryptor.isEncrypted(plainTextValue)) {
+					System.err.println(key.trim() + " is already encrypted.");
+					canEncrypt = (newCipherKeyGenerated) ? false : true;
+				}
+			}
+
+			if (!canEncrypt) {
+				String msg = "Input configuration file has problems with prior encryptions, cannot continue the encrypt";
+				System.err.println(msg);
+				throw new Exception(msg);
+			}
+
+			boolean propertiesChanged = false;
+			for (String key : secureProperties) {
+				String plainTextValue = properties.get(key.trim());
+				String value = cpsEncryptor.encrypt(plainTextValue);
+				properties.put(key, value);
+				propertiesChanged = true;
+			}
+			if (propertiesChanged) {
+				if (newCipherKeyGenerated) {
+					payload.put("cipherKey", cipherKey);
+				}
+				payload.put("properties", properties);
+			} else {
+				System.err.println("No properties encrypted");
+			}
+		}
+
+		return new ObjectMapper().writerWithDefaultPrettyPrinter().writeValueAsString(payload);
+	}
+
+	private static String decrypt(String data) throws Exception {
+		Map<String, Object> payload;
+		ObjectMapper mapper;
+		TypeFactory factory;
+		MapType type;
+
+		factory = TypeFactory.defaultInstance();
+		type = factory.constructMapType(LinkedHashMap.class, String.class, Object.class);
+		mapper = new ObjectMapper();
+		payload = mapper.readValue(data, type);
+
+		String keyId = (String) payload.get("keyId");
+		String cipherKey = (String) payload.get("cipherKey");
+		if (keyId == null || keyId.isEmpty()) {
+			String msg = "Need a keyId to be specified in the configuration file, cannot continue with decrypt";
+			System.err.println(msg);
+			throw new Exception(msg);
+		}
+		
+		Map<String, String> properties = (Map<String, String>) payload.get("properties");
+		boolean priorEncryptions = false;
+		for (String key : properties.keySet()) {
+			String value = properties.get(key);
+			if (value.startsWith("![")) {
+				priorEncryptions = true;
+			}
+		}
+		if (!priorEncryptions) {
+			return data;
+		}
+
+		if (cipherKey == null || cipherKey.isEmpty()) {
+			String msg = "Need a cipherKey to be specified in the configuration file, cannot continue with decrypt";
+			System.err.println(msg);
+			throw new Exception(msg);
+		}
+
+		CpsEncryptor cpsEncryptor = new CpsEncryptor(keyId, cipherKey);
+		for (String key : properties.keySet()) {
+			String encryptedValue = properties.get(key);
+			String value = cpsEncryptor.decrypt(encryptedValue);
+			properties.put(key, value);
+		}
+		payload.remove("cipherKey");
+
+		return new ObjectMapper().writerWithDefaultPrettyPrinter().writeValueAsString(payload);
+	}
+
+	private static void printHelp() {
+		System.out.println("\nUsage: java -jar CpsTool {operation} [parameters]\n");
+		System.out.println("  operations:");
+		System.out.println("    decrypt           Read stdin and decrypt to stdout");
+		System.out.println("    encrypt           Read stdin and encrypt to stdout");
+		System.out.println("      parameters:");
+		System.out.println(
+				"        keyId         The keyId to use for unencrypted file, will be ignored if the file contains encrypted data");
+		System.out.println("    rotateKey         Read stdin and re-encrypt with new key to stdout");
+		System.out.println("      parameters:");
+		System.out.println("        keyId         The keyId to use for new encryption (required)");
+		System.out.println("\n");
+	}
+}
